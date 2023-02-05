7ffb5711cb5695dca375de54fa2733a263666a41
Holden Cormier <hcormie2@villanova.edu>
2023-02-04 23:02:17 -0500

    first commit

---
+++ marketplace-api/src/test/java/com/example/marketplaceapi/MarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketplaceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
