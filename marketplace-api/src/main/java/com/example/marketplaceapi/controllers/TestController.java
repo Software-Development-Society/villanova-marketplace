1a973b50e71d57a71ba28f18743791d8d1b0d273
Holden Cormier <hcormie2@villanova.edu>
2023-02-06 21:58:59 -0500

    adding controllers

--- marketplace-api/src/main/java/com/example/marketplaceapi/controllers/TestController.java
+++ marketplace-api/src/main/java/com/example/marketplaceapi/controllers/TestController.java
@@ -1,10 +1,12 @@
 package com.example.marketplaceapi.controllers;
 
+import groovy.json.JsonSlurper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static final JsonSlurper slurper =  new JsonSlurper();
 
     @GetMapping("/init-test")
     public String initTest(){
