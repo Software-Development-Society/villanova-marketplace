3258385b998bae6c1f729520a47ac351b26d8bb1
Holden Cormier <hcormie2@villanova.edu>
2023-02-07 12:35:43 -0500

    adding some sample methods

--- marketplace-api/src/main/java/com/example/marketplaceapi/controllers/TestController.java
+++ marketplace-api/src/main/java/com/example/marketplaceapi/controllers/TestController.java
@@ -1,16 +1,25 @@
 package com.example.marketplaceapi.controllers;
 
 import groovy.json.JsonSlurper;
+import org.bson.types.ObjectId;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
 
 @RestController
 public class TestController {
     private static final JsonSlurper slurper =  new JsonSlurper();
 
-    @GetMapping("/init-test")
-    public String initTest(){
+
+    // EXAMPLE API ENDPOINT - GET METHOD
+    @GetMapping("/init-test") // GetMapping states the type of method and the "/...." is the url endpoint
+    public String initTest() {
         System.out.println("Everything looks good!");
+        // logic done here
         return "Looks good!";
 
     }
--- marketplace-api/src/main/java/com/example/marketplaceapi/services/DatabaseServices.java
+++ marketplace-api/src/main/java/com/example/marketplaceapi/services/DatabaseServices.java
@@ -1,4 +1,27 @@
 package com.example.marketplaceapi.services;
 
 public class DatabaseServices {
+
+
+
+//    // EXAMPLE METHOD TO GET STUDENT FROM DATABASE
+//    public static Student getStudentUsingId(ObjectId user_id){
+//        // for each loop to get each student from the db
+//        for(Student student : visableStudentRepo.findAll()){
+//            // compare the current id to the target id
+//            if(student.getStudentId().equals(user_id)){
+//                return student;
+//            }
+//
+//        }
+//        // if it gets here there are big problems
+//        return new Student();
+//    }
+
+
+
+
+
+
+
 }
