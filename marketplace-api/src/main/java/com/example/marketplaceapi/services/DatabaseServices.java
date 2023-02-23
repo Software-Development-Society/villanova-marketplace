529748b2a161fe161dd28e5923e4d334b8c1a08f
Holden Cormier <hcormie2@villanova.edu>
2023-02-23 09:30:39 -0500

    adding comments to clarify database services

--- marketplace-api/src/main/java/com/example/marketplaceapi/services/DatabaseServices.java
+++ marketplace-api/src/main/java/com/example/marketplaceapi/services/DatabaseServices.java
@@ -17,6 +17,23 @@ import java.util.List;
 public class DatabaseServices {
 
 
+    //    // EXAMPLE METHOD TO GET STUDENT FROM DATABASE
+//    public static Student getStudentUsingId(ObjectId user_id){
+//        // for each loop to get each student from the db
+//        for(Student student : visableStudentRepo.findAll()){
+//            // compare the current id to the target id
+//            if(student.getStudentId().equals(user_id)){
+//                return student;
+//            }
+//
+//        }
+//        // if it gets here there are big problems
+//        System.out.println("There is no student with id: " +user_id);
+//        return new Student();
+//    }
+
+
+
     public static List<User> getAllUsers(){
 
         return new ArrayList<>(MarketplaceApiApplication.visableUserRepo.findAll());
@@ -65,20 +82,6 @@ public class DatabaseServices {
 
 
 
-//    // EXAMPLE METHOD TO GET STUDENT FROM DATABASE
-//    public static Student getStudentUsingId(ObjectId user_id){
-//        // for each loop to get each student from the db
-//        for(Student student : visableStudentRepo.findAll()){
-//            // compare the current id to the target id
-//            if(student.getStudentId().equals(user_id)){
-//                return student;
-//            }
-//
-//        }
-//        // if it gets here there are big problems
-//        System.out.println("There is no student with id: " +user_id);
-//        return new Student();
-//    }
 
 
 
