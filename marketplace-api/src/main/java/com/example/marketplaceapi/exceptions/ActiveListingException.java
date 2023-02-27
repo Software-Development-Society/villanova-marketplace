b53e3756bf188a1065735472f4ea574a2f1c279c
Holden Cormier <hcormie2@villanova.edu>
2023-02-27 09:00:22 -0500

    added activelisting exception

---
+++ marketplace-api/src/main/java/com/example/marketplaceapi/exceptions/ActiveListingException.java
@@ -0,0 +1,8 @@
+package com.example.marketplaceapi.exceptions;
+
+public class ActiveListingException extends Exception{
+
+    public ActiveListingException(String message) {
+        super(message);
+    }
+}
