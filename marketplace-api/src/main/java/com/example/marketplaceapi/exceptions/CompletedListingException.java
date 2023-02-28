1397597328cbb4b113f1e051f53d4eb513c57e71
Holden Cormier <hcormie2@villanova.edu>
2023-02-28 09:01:04 -0500

    added completedlisting exception

---
+++ marketplace-api/src/main/java/com/example/marketplaceapi/exceptions/CompletedListingException.java
@@ -0,0 +1,7 @@
+package com.example.marketplaceapi.exceptions;
+
+public class CompletedListingException extends Exception{
+    public CompletedListingException(String message) {
+        super(message);
+    }
+}
