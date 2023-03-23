72d1ff7971072f0cbea6a3964a496c778b52021a
dbw7 <90856056+dbw7@users.noreply.github.com>
2023-03-22 20:00:33 -0400

    updated form

--- marketplace-app/villanova-marketplace/src/components/RegisComp/RegisForm/RegisForm.jsx
+++ marketplace-app/villanova-marketplace/src/components/RegisComp/RegisForm/RegisForm.jsx
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import RegisInput from "../RegisInput/RegisInput";
 import "./RegisForm.css"
 
 const RegisForm = () => {
+    const [validForm, setValidForm] = useState(false);
+    
     const [formInfo, setFormInfo] = useState({
         firstName: "",
         lastName: "",
-        age: 0
+        major:"",
+        classYear: 0
     });
     
-    const formHandler = () => {
-        
-    }
+    useEffect(()=>{
+        //console.log(formInfo.firstName.length, formInfo.lastName.length, formInfo.classYear, formInfo.major.length)
+        if(formInfo.firstName.length > 2 && formInfo.lastName.length > 1 && (formInfo.classYear === "2023" || formInfo.classYear === "2024" || formInfo.classYear === "2025" || formInfo.classYear === "2026") && formInfo.major.length > 2){
+            setValidForm(true);
+        } else {
+            setValidForm(false);
+        }
+    }, [formInfo])
+    
     
     return (
         <form className="form-box">
-            <RegisInput type="text" label="First Name" ></RegisInput>
-            <RegisInput type="text" label="Last Name" ></RegisInput>
-            <RegisInput type="number" label="Age" ></RegisInput>
-            <button>Submit</button>
+            <RegisInput objVal="firstName" type="text" label="First Name" setFormInfo={setFormInfo} ></RegisInput>
+            <RegisInput objVal="lastName" type="text" label="Last Name" setFormInfo={setFormInfo} ></RegisInput>
+            <RegisInput objVal="major" type="text" label="Major" setFormInfo={setFormInfo} ></RegisInput>
+            <RegisInput objVal="classYear" type="number" label="Class Year" setFormInfo={setFormInfo}></RegisInput>
+            <button disabled={!validForm} >Submit</button>
+            {JSON.stringify(formInfo)}
         </form>
     )
 }
--- marketplace-app/villanova-marketplace/src/components/RegisComp/RegisInput/RegisInput.jsx
+++ marketplace-app/villanova-marketplace/src/components/RegisComp/RegisInput/RegisInput.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import "./RegisInput.css"
 
 const RegisInput = (props) => {
+    const valHandler = (event) => {
+        //console.log(event.target.value)
+        props.setFormInfo((prevState)=>{
+            return {
+                ...prevState,
+                [props.objVal]: event.target.value,
+            }
+        })
+    }
+    
     return(
         <>
             <label for={props.label}> {props.label} </label>
-            <input type={props.type} name={props.label}></input>
+            <input type={props.type} name={props.label} onChange={valHandler}></input>
         </>
     )
 }
