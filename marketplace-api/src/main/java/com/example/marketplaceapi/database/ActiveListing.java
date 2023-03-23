0a41722b9ef9a8fb32905bfa0d6256df97b049f2
Holden Cormier <hcormie2@villanova.edu>
2023-03-23 12:01:09 -0400

    adding set, get, and toString to active listing

--- marketplace-api/src/main/java/com/example/marketplaceapi/database/ActiveListing.java
+++ marketplace-api/src/main/java/com/example/marketplaceapi/database/ActiveListing.java
@@ -87,12 +87,107 @@ public class ActiveListing {
 
     // add other get methods here
 
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public ArrayList<Binary> getPictures() {
+        return pictures;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public ObjectId getSeller_id() {
+        return seller_id;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
 
 
     // example set method
-
     public void setListing_id(ObjectId listing_id) {
         this.listing_id = listing_id;
     }
     // add other set methods here
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setPictures(ArrayList<Binary> pictures) {
+        this.pictures = pictures;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public void setSeller_id(ObjectId seller_id) {
+        this.seller_id = seller_id;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    @Override
+    public String toString() {
+        return "ActiveListing{" +
+                "listing_id=" + listing_id +
+                ", productName='" + productName + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", pictures=" + pictures +
+                ", sellerName='" + sellerName + '\'' +
+                ", seller_id=" + seller_id +
+                ", tags=" + tags +
+                ", views=" + views +
+                ", condition='" + condition + '\'' +
+                ", dateAdded=" + dateAdded +
+                '}';
+    }
 }
