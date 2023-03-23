b58f34f0b43d328829d7df4cb1b3f7aa8ef45081
Preston Button <prestonbutton99@gmail.com>
2023-03-22 20:20:40 -0400

    get spacing figured out for desktop

--- marketplace-app/villanova-marketplace/src/components/Navbar/Navbar.jsx
+++ marketplace-app/villanova-marketplace/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -9,77 +8,9 @@ import Button from "@material-ui/core/Button";
 import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from "@material-ui/icons/Search";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-  },
-  title: {
-    flexGrow: 1,
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: theme.palette.common.white,
-    "&:hover": {
-      backgroundColor: theme.palette.common.white,
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto",
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-  navLinks: {
-    display: "flex",
-    justifyContent: "flex-end",
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
-  },
-  mobileMenu: {
-    display: "none",
-    [theme.breakpoints.down("sm")]: {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      width: "100%",
-    },
-  },
-}));
-
-function Navbar() {
-  const classes = useStyles();
+export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const handleMobileMenuOpen = () => {
@@ -91,49 +22,48 @@ function Navbar() {
   };
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            My App
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              {/* Search icon here */ <SearchIcon />}
+    <div className="root">
+      <AppBar position="static" className="AppBar">
+        <Toolbar className="toolbar">
+          <div className="search">
+            <div className="searchIcon">
+              <SearchIcon />
             </div>
             <InputBase
               placeholder="Search…"
               classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
+                root: "inputRoot",
+                input: "inputInput",
               }}
               inputProps={{ "aria-label": "search" }}
             />
           </div>
-          <div className={classes.navLinks}>
-            <Button color="inherit" component={Link} to="/">
-              Home
-            </Button>
-            <Button color="inherit" component={Link} to="/marketplace">
-              Marketplace
-            </Button>
-            <Button color="inherit" component={Link} to="/account">
-              Account
-            </Button>
+          <div className="wrapperDiv">
+            <div className="navLinks">
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/marketplace">
+                Marketplace
+              </Button>
+              <Button color="inherit" component={Link} to="/account">
+                Account
+              </Button>
+            </div>
+            <IconButton
+              edge="end"
+              className="menuButton"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleMobileMenuOpen}
+            >
+              <MenuIcon />
+            </IconButton>
           </div>
-          <IconButton
-            edge="end"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-            onClick={handleMobileMenuOpen}
-          >
-            <MenuIcon />
-          </IconButton>
         </Toolbar>
       </AppBar>
       <div
-        className={classes.mobileMenu}
+        className="mobileMenu"
         style={{ display: mobileMenuOpen ? "flex" : "none" }}
       >
         <Button
