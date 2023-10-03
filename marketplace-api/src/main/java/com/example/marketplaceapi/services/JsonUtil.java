0f83e403f750fec3f985cad914bf21dfcc40ddf5
chrismarquezz <chrismarquez00@icloud.com>
2023-10-03 18:48:12 -0400

    Added Class and Method

--- marketplace-api/src/main/java/com/example/marketplaceapi/services/JsonUtil.java
+++ marketplace-api/src/main/java/com/example/marketplaceapi/services/JsonUtil.java
@@ -5,18 +5,6 @@ import org.bson.json.JsonObject;
 
 public class JsonUtil {
     public static ActiveListing getActiveListingFromJson(JsonObject Json) {
-        String[] variables = {"listing_id", "productName", "description", "price", "pictures", "sellerName", "seller_id", "tags", "views", "condition", "dateAdded"};
-
-        for (int i = 0; i < variables.length; i++) {
-            
-        }
-        if (Json.get("listing_id") != null)    {
-
-        }
-        else {
-
-        }
-
-
+        try 
     }
 }
