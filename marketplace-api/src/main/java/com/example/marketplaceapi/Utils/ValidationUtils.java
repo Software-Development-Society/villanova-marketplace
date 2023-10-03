d1235a64dce2c217640462e45b7f89a296aba465
jfacinelli <jfacinel@villanova.edu>
2023-10-03 19:25:44 -0400

    Created validateCreateActiveListing method
    Method validates that the active listing being created isn't null and isn't already in the database.

--- marketplace-api/src/main/java/com/example/marketplaceapi/Utils/ValidationUtils.java
+++ marketplace-api/src/main/java/com/example/marketplaceapi/Utils/ValidationUtils.java
@@ -1,5 +1,6 @@
 package com.example.marketplaceapi.Utils;
 
+import com.example.marketplaceapi.database.ActiveListing;
 import com.example.marketplaceapi.database.User;
 import com.example.marketplaceapi.exceptions.SvcException;
 import com.example.marketplaceapi.services.DatabaseServices;
@@ -13,6 +14,16 @@ public class ValidationUtils {
 
         if (DatabaseServices.findUserById(pUser.getUser_id()).isPresent()){
             throw new SvcException("User already exists");
+        }
+    }
+
+    public static void validateCreateActiveListing(final ActiveListing pActiveListing) throws SvcException{
+        if(pActiveListing == null){
+            throw new SvcException("Active Listing is null");
+        }
+
+        if (DatabaseServices.findActiveListingById(pActiveListing.getListing_id()).isPresent()){
+            throw new SvcException("Active Listing already exists");
         }
     }
 }
