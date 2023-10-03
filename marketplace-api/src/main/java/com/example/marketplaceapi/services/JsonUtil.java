8e0700587f6c1c6768ee30679cf6ab5e1496cfaf
chrismarquezz <chrismarquez00@icloud.com>
2023-10-03 19:11:45 -0400

    Created MOST of ActiveListing JSON

--- marketplace-api/src/main/java/com/example/marketplaceapi/services/JsonUtil.java
+++ marketplace-api/src/main/java/com/example/marketplaceapi/services/JsonUtil.java
@@ -1,10 +1,35 @@
 package com.example.marketplaceapi.services;
 
 import com.example.marketplaceapi.database.ActiveListing;
-import org.bson.json.JsonObject;
+import org.bson.types.Binary;
+import org.bson.types.ObjectId;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class JsonUtil {
-    public static ActiveListing getActiveListingFromJson(JsonObject Json) {
-        try 
+    public static ActiveListing getActiveListingFromJson(JSONObject Json) {
+        ActiveListing activeListing = new ActiveListing();
+
+        try {
+            activeListing.setListing_id((ObjectId) Json.get("listing_id"));
+            activeListing.setProductName((String) Json.get("productName"));
+            activeListing.setDescription((String) Json.get("description"));
+            activeListing.setPrice((int) Json.get("price"));
+            activeListing.setPictures((ArrayList<Binary>) Json.get("pictures"));
+            activeListing.setSellerName((String) Json.get("sellerName"));
+            activeListing.setSeller_id((ObjectId) Json.get("seller_id"));
+            activeListing.setTags((ArrayList<String>) Json.get("tags"));
+            activeListing.setViews((int) Json.get("views"));
+            activeListing.setCondition((String) Json.get("condition"));
+            activeListing.setDateAdded((Date) Json.get("date"));
+        }
+        catch(Exception e) {
+            // throw new ActiveListing();
+        }
+
+        return activeListing;
+
     }
 }
